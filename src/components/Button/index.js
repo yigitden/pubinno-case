ae159622da5baecb7ab3023615684ecec2a0e0e4
yigitden <yigitcandeniz@gmail.com>
2022-06-23 15:40:41 +0300

    default data changed and added truncate function

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import { ButtonStyle } from './Button.style';
-const index = ({text,onClick,startIcon,color}) => {
+import React from "react";
+import Button from "@mui/material/Button";
+import { ButtonStyle } from "./Button.style";
+const index = ({ text, onClick, startIcon, color }) => {
   return (
-    <Button variant="contained" color={color} onClick={onClick} startIcon={startIcon} sx={ButtonStyle.button}>{text}</Button>
-  )
-}
+    <Button
+      variant="contained"
+      color={color}
+      onClick={onClick}
+      startIcon={startIcon}
+      sx={ButtonStyle.button}
+    >
+      {text}
+    </Button>
+  );
+};
 
-export default index
+export default index;
--- src/components/Table/TableItem.js
+++ src/components/Table/TableItem.js
@@ -1,22 +1,25 @@
-import { TableCell, TableRow} from '@mui/material'
-import React from 'react'
-import { TableStyle } from './Table.style'
+import { TableCell, TableRow } from "@mui/material";
+import React from "react";
+import { TableStyle } from "./Table.style";
 
-const TableItem = ({row,handleOpen}) => {
+const TableItem = ({ row, handleOpen }) => {
+  function truncateString(str, num) {
+    //truncate function for long address
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  }
+
   return (
-  
-    <TableRow
-    onClick={handleOpen}
-    key={row.id}
-    sx={TableStyle.table_item}
-  >
-    <TableCell component="th" scope="row">
-      {row.name}
-    </TableCell>
-    <TableCell align="left">{row.address}</TableCell>
-  </TableRow>
-  
-  )
-}
+    <TableRow onClick={handleOpen} key={row.id} sx={TableStyle.table_item}>
+      <TableCell component="th" scope="row">
+        {row.name}
+      </TableCell>
+      <TableCell align="left">{truncateString(`${row.address}`, 70)}</TableCell>
+    </TableRow>
+  );
+};
 
-export default TableItem
+export default TableItem;
